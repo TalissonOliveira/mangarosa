4f855814a7c7a67d33d002f4d6f9b193225736ea
Talisson Oliveira <talissonlux@outlook.com>
2021-11-08 04:07:42 -0300

    feat: shows employees dynamically

--- src/pages/Records/index.js
+++ src/pages/Records/index.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react'
 import { Header } from '../../components/Header'
 import styles from './styles.module.scss'
 
-export function Records() {
+export function Records(props) {
+    const [employees, setEmployees] = useState([])
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        fetch('http://localhost:3333/employee', options)
+            .then(response => response.json())
+            .then(data => setEmployees(data))
+            .catch(error => console.log(error))
+    }, [])
+
     return (
         <main className={styles.container}>
             <Header>
@@ -9,30 +26,26 @@ export function Records() {
             </Header>
             <table className={styles.table}>
                 <thead className={styles.tableHead}>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>CPF</th>
-                    <th>Status</th>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>CPF</th>
+                        <th>Status</th>
+                    </tr>
                 </thead>
                 <tbody className={styles.tableBody}>
-                    <tr>
-                        <td>1</td>
-                        <td>Talisson Olasdasdasdeira de Souza</td>
-                        <td>123.123.123-99</td>
-                        <td>Não validado</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Talisson Oliveira de Souza</td>
-                        <td>123.123.123-99</td>
-                        <td>Não validado</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Talisson Oliveira de Souza</td>
-                        <td>123.123.123-99</td>
-                        <td>Não validado</td>
-                    </tr>
+                    {employees.length > 0 ?
+                        employees.map(employee => (
+                            <tr>
+                                <td>{employee.id}</td>
+                                <td>{employee.name}</td>
+                                <td>{employee.cpf}</td>
+                                <td>{employee.valid ? 'Válido' : 'Não válido'}</td>
+                            </tr>
+                        ))
+                        :
+                        <p>Nada por aqui</p>
+                    }
                 </tbody>
             </table>
         </main>
